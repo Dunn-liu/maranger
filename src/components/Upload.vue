1cab4be163d1c03ae58d48ac80986d5391f56274
v_vqqdliu <v_vqqdliu@tencent.com>
2021-07-23 18:41:18 +0800

    feat:优化

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,44 +1,45 @@
 <template>
-<el-upload
-    class="avatar-uploader"
-    action="/api/upload/image"
-    :show-file-list="false"
-    :http-request="httpRequest"
->
-<img v-if="src" :src="src" class="avatar">
-<i v-else class="el-icon-plus avatar-uploader-icon"></i>
-</el-upload>
+  <div class="upload_com">
+    <el-upload
+      class="avatar-uploader"
+      action="/api/upload/image"
+      :drag="true"
+      :show-file-list="false"
+      :http-request="httpRequest"
+    >
+      <img v-if="src" :src="src" class="avatar" />
+      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+    </el-upload>
+    <el-input v-model="input" placeholder="请输入图片描述"></el-input>
+  </div>
 </template>
 <script>
-import {defineComponent,watch} from 'vue'
-import useImgRequest  from '../utils/imgHttpRequest'
+import { defineComponent, watch } from "vue";
+import useImgRequest from "../utils/imgHttpRequest";
 export default defineComponent({
-  emits:['getSrc'],
-  props:['src'],
-  setup(props,context){
-    const { httpRequest , resSrc} = useImgRequest()
-      watch(resSrc,(newVal)=>{
-          context.emit('getSrc',newVal)
-          console.log('resSrc.value',newVal)
-      })
-    return{
-      httpRequest
-    }
-  }
-})
+  emits: ["getSrc"],
+  props: ["src"],
+  setup(props, context) {
+    const { httpRequest, resSrc } = useImgRequest();
+    watch(resSrc, (newVal) => {
+      context.emit("getSrc", newVal);
+    });
+    return {
+      httpRequest,
+    };
+  },
+});
 </script>
-<style>
-.avatar-uploader .el-upload {
-  border: 1px dashed #d9d9d9;
-  border-radius: 6px;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
+<style lang='scss' scoped>
+.upload_com {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
-.avatar-uploader .el-upload:hover {
-  border-color: #409EFF;
+:deep .avatar-uploader .el-upload:hover {
+  border-color: #409eff;
 }
-.avatar-uploader-icon {
+:deep .avatar-uploader-icon {
   font-size: 28px;
   color: #8c939d;
   width: 178px;
@@ -46,7 +47,7 @@ export default defineComponent({
   line-height: 178px;
   text-align: center;
 }
-.avatar {
+:deep .avatar {
   width: 178px;
   height: 178px;
   display: block;
