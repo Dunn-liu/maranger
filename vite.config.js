dd36e41c826a000ac4f8f741cbf114471308a50a
Dunn <15817294245@163.com>
2021-07-23 14:34:05 +0800

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'./',
+  base:'/',
   alias: {
     '@': path.resolve(__dirname, './src')
   },
