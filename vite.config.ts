b7f0213f51526d4a8d39d8d0ff3a739ba0523488
DunnLiu <15817294245@163.com>
2022-05-02 11:11:51 +0800

    feat:开发地址改变

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://test.liuqidong.com',
         changeOrigin: true,
         rewrite:(path)=>path.replace(/^\/api/,'')
       }
